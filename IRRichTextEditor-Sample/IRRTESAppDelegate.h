f7bd75423511ba753b3047b9f25ab8e06409cea7
Evadne Wu <ev@monoceroi.com>
2012-06-25 21:17:48 +0800

    bare basics that actually works

---
+++ IRRichTextEditor-Sample/IRRTESAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IRRTESAppDelegate.h
+//  IRRichTextEditor-Sample
+//
+//  Created by Evadne Wu on 6/25/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IRRTESAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ IRRichTextEditor-Sample/IRRTESAppDelegate.m
@@ -0,0 +1,26 @@
+//
+//  IRRTESAppDelegate.m
+//  IRRichTextEditor-Sample
+//
+//  Created by Evadne Wu on 6/25/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import "IRRTESAppDelegate.h"
+#import "IRRichTextEditorViewController.h"
+
+@implementation IRRTESAppDelegate
+@synthesize window = _window;
+
+- (BOOL) application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+
+	//	self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+	//	self.window.rootViewController = [[IRRichTextEditorViewController alloc] init];
+	//	
+	//	[self.window makeKeyAndVisible];
+	
+	return YES;
+	
+}
+
+@end
