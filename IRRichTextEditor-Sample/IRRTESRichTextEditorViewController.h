ba8896b4e3e680f627ee2438d4a0d0bbb7f606b5
Evadne Wu <ev@monoceroi.com>
2012-06-25 23:25:02 +0800

    updates IRRichTextEditor, storyboard

---
+++ IRRichTextEditor-Sample/IRRTESRichTextEditorViewController.h
@@ -0,0 +1,15 @@
+//
+//  IRRTESRichTextEditorViewController.h
+//  IRRichTextEditor-Sample
+//
+//  Created by Evadne Wu on 6/25/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import "IRRichTextEditorViewController.h"
+
+@interface IRRTESRichTextEditorViewController : IRRichTextEditorViewController
+
+- (IBAction) handleDone:(id)sender;
+
+@end
---
+++ IRRichTextEditor-Sample/IRRTESRichTextEditorViewController.m
@@ -0,0 +1,23 @@
+//
+//  IRRTESRichTextEditorViewController.m
+//  IRRichTextEditor-Sample
+//
+//  Created by Evadne Wu on 6/25/12.
+//  Copyright (c) 2012 Iridia Productions. All rights reserved.
+//
+
+#import "IRRTESRichTextEditorViewController.h"
+
+@interface IRRTESRichTextEditorViewController ()
+
+@end
+
+@implementation IRRTESRichTextEditorViewController
+
+- (IBAction)handleDone:(id)sender {
+
+	//	TBD
+
+}
+
+@end
